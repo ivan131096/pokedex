4fabc1f7d75e64359caf49a453b8874f8ef99dd8
ivan131096 <ivanrockemback@gmail.com>
2025-05-26 21:43:28 -0300

    pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
 import {Button} from 'primeng/button';
 import {Image} from 'primeng/image';
-import {DataView} from 'primeng/dataview';
 import {Paginator, PaginatorState} from 'primeng/paginator';
-import {NgClass, NgForOf, NgOptimizedImage, NgStyle} from '@angular/common';
-import {clearInterval} from 'node:timers';
+import {FormsModule} from '@angular/forms';
+import {InputText} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button, Image, DataView, Paginator, NgForOf, NgOptimizedImage, NgStyle, NgClass],
+  imports: [Button, Image, Paginator, FormsModule, InputText],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,39 +15,27 @@ export class AppComponent implements OnInit {
   title = 'PokeDex';
   list:any[] = [];
   first: number = 0;
-  rows: number = 10;
+  rows: number = 30;
   totalRecords: number = 0;
-  test:any;
+  nome:string = ''
+
+  ngOnInit() {
+    this.fetchAll()
+  }
 
   onPageChange(event: PaginatorState) {
     const offset = (event.rows! * event.page!)
     this.fetchAll(offset, event.rows!)
   }
 
-  ngOnInit() {
-    this.fetchAll()
-  }
-
-  demonho(){
-    console.log(`satanas`)
-  }
-
-  demon: any
-
-  fetchAll(offset: number = 0, limit: number = 10) {\
-    this.clear()
+  fetchAll(offset: number = 0, limit: number = 30) {
     fetch('https://pokeapi.co/api/v2/pokemon?'+`offset=`+offset+`&`+`limit=`+limit).then(res => res.json()).then((data) => {
       data.results.forEach((element:any) => {
         this.totalRecords = data.count;
         this.list = []
         this.fetchOne(element.url)
       })
-      this.demon = setInterval(this.demonho, 1000)
     })
-  }
-
-  clear(){
-    clearInterval(this.demon)
   }
 
   fetchOne(url:any){
